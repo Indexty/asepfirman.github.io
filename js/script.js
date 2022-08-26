9a6bcc3bc2cdfca251c57834ac335ab989cc59ef
4SEP30 <asepfirman811@gmail.com>
2022-08-26 20:08:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let span = document.querySelector('ul span');
+let nav = document.querySelector('ul li');
+let btn = document.querySelector('.buton');
+let clip = document.querySelector('.clip');
+nav.addEventListener('click', function () {
+    span.style.display = 'inline';
+})
+
+window.addEventListener("scroll", function () {
+    var nav = this.document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+btn.onclick = function () {
+    btn.classList.add('active');
+    clip.classList.add('active');
+}
+
+
